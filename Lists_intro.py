ec78c99b37bc4cd810c00bd9b7a9535befcc0df2
Peter Kimberley <p3t3rk1m80@gmail.com>
2022-01-24 18:36:49 +0000

    These are examples of muttable and imuttable objects within Python

--- Lists_intro.py
+++ Lists_intro.py
@@ -11,7 +11,8 @@ print()
 print (computer_parts [2])
 print()
 print(computer_parts[0:3])
-print(computer_parts[-1]) # This is a python idyom
+print(computer_parts[-1]) # This is a python idyom that will print the
+# last item in a list
 
 # String are immutable hower Lists are as you can change the contents
 # of a list
--- mutable.py
+++ mutable.py
@@ -18,9 +18,14 @@ print(id(shopping_list))
 print(another_list)
 print()
 a = b = c = d = f = another_list
+# The above is a multiple names e.g.(a = b =)
+# to the variable (another list)
 print(a)
 print()
 print("Adding cream")
 b.append("cream")
+# b.append Cream has noe added Cream to the list, so any more calls to
+# print the list will add Cream on the end after this point in the code
 print(c)
 print(d)
+  
